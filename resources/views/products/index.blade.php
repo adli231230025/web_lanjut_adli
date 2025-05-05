481de1e760726bea2c2d20f416aa30e4f4ed5d09
Nama Kamu <emailkamu@example.com>
2025-05-05 12:21:24 +0700

    Admin Proyek

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,24 +1,42 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Produk E-Commerce</title>
+    <title>Contact Form</title>
 </head>
-
 <body>
-    <h1>Daftar Produk</h1>
+    <h2>Contact us</h2>
 
-    <ul>
-        @foreach($products as $id => $product)
-        <li>
-            <a href="{{ route('products.show', $id) }}">
-                {{ $product['name'] }} - Rp{{ number_format($product['price'], 0, ',', '.') }}
-            </a>
-        </li>
-        @endforeach
-    </ul>
+    @if(session('succes'))
+        <p style="color:green">{{session('succes') }}</p>
+    @endif
 
-    
+    @if ($errors->any())
+        <div style="color:red">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $errors }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <Form action="/contact" method="POST">
+        @csrf 
+        <label>Name:</label><br>
+        <input type="text" name="name" value="{{ old('name') }}"><br><br>
+
+        <label>Email:</label><br>
+        <input type="email" name="email" value="{{ old('email') }}"><br><br>
+
+        <label>No.hp:</label><br>
+        <input type="text" name="phone" value="{{ old('phone') }}"><br><br>
+
+        <label>Message:</label><br>
+        <textarea name="message">{{ old('message') }}</textarea><br><br>
+
+
+        <button type="Submit">Kirim</button>
+    </Form>
 </body>
-
+    
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AdminController;
 use Livewire\Volt\Volt;
 
 // Route untuk menampilkan daftar produk
@@ -9,3 +10,8 @@ Route::get('/', [ProductController::class, 'index'])->name('products.index');
 
 // Route untuk menampilkan detail produk
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+
+
+
+Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
+Route::post('/admin/store', [AdminController::class, 'store'])->name('admin.store');
