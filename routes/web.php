3408b25dc864442acdb1d2e91263a3fcb796b465
Nama Kamu <emailkamu@example.com>
2025-05-18 22:55:59 +0700

    Membuat halaman di dalam bladet

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminController;
+
 use Livewire\Volt\Volt;
 
 // Route untuk menampilkan daftar produk
@@ -12,6 +14,25 @@ Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 
 
+Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 
-Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
-Route::post('/admin/store', [AdminController::class, 'store'])->name('admin.store');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+Route::get('/admin', [AdminController::class, 'create']);
+Route::post('/admin', [AdminController::class, 'store'])->name('admin.store');
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
+});
